72c0e6930541864e9fcf5610966cf4a3b27112cb
chisholmfps <jchish9900@gmail.com>
2025-06-24 23:25:49 -0400

    just some quality control stuff

---
+++ script.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+    $('#ripple-header').ripples({
+      resolution: 256,
+      dropRadius: 20,
+      perturbance: 0.03
+    });
+  });
+
+
+
+
+
+
+
+
+  
+
+function rotateCarousel(className) {
+  let current = 0;
+  const images = document.querySelectorAll(`.${className}`);
+
+  setInterval(() => {
+    images[current].classList.remove('active');
+    current = (current + 1) % images.length;
+    images[current].classList.add('active');
+  }, 3000);
+}
+
+rotateCarousel('carousel1');
+rotateCarousel('carousel2');
+
+
+
+
+
+function showTab(tabId) {
+    document.querySelectorAll('.tab').forEach(tab => {
+      tab.classList.remove('active');
+    });
+
+    document.querySelectorAll('.tab-content').forEach(content => {
+      content.classList.remove('active');
+    });
+
+    const activeTabButton = document.querySelector(`.tab[onclick="showTab('${tabId}')"]`);
+    if (activeTabButton) {
+      activeTabButton.classList.add('active');
+    }
+
+    const activeTabContent = document.getElementById(tabId);
+    if (activeTabContent) {
+      activeTabContent.classList.add('active');
+    }
+  }
